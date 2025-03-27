b10f46b9100075a5984e8969edf160692dc846b5
Nir Bar <nir.bar@panel-sw.co.il>
2025-03-27 17:16:27 +0200

    Disable deleting paylaods / containers from their unverified path

--- src/burn/engine/cache.cpp
+++ src/burn/engine/cache.cpp
@@ -1746,7 +1746,7 @@ static HRESULT VerifyThenTransferContainer(
     HANDLE hFile = INVALID_HANDLE_VALUE;
 
     // Get the container on disk actual hash.
-    hFile = ::CreateFileW(wzUnverifiedContainerPath, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_DELETE, NULL, OPEN_EXISTING, FILE_FLAG_SEQUENTIAL_SCAN, NULL);
+    hFile = ::CreateFileW(wzUnverifiedContainerPath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_FLAG_SEQUENTIAL_SCAN, NULL);
     if (INVALID_HANDLE_VALUE == hFile)
     {
         ExitWithLastError(hr, "Failed to open container in working path: %ls", wzUnverifiedContainerPath);
@@ -1766,6 +1766,10 @@ static HRESULT VerifyThenTransferContainer(
 
     LogStringLine(REPORT_STANDARD, "%ls container from working path '%ls' to path '%ls'", fMove ? L"Moving" : L"Copying", wzUnverifiedContainerPath, wzCachedPath);
 
+    if (fMove)
+    {
+        ReleaseFileHandle(hFile);
+    }
     hr = CacheTransferFileWithRetry(wzUnverifiedContainerPath, wzCachedPath, fMove, BURN_CACHE_STEP_FINALIZE, pContainer->qwFileSize, pfnCacheMessageHandler, pfnProgress, pContext);
 
 LExit:
@@ -1788,7 +1792,7 @@ static HRESULT VerifyThenTransferPayload(
     HANDLE hFile = INVALID_HANDLE_VALUE;
 
     // Get the payload on disk actual hash.
-    hFile = ::CreateFileW(wzUnverifiedPayloadPath, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_DELETE, NULL, OPEN_EXISTING, FILE_FLAG_SEQUENTIAL_SCAN, NULL);
+    hFile = ::CreateFileW(wzUnverifiedPayloadPath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_FLAG_SEQUENTIAL_SCAN, NULL);
     if (INVALID_HANDLE_VALUE == hFile)
     {
         ExitWithLastError(hr, "Failed to open payload in working path: %ls", wzUnverifiedPayloadPath);
@@ -1812,6 +1816,10 @@ static HRESULT VerifyThenTransferPayload(
 
     LogStringLine(REPORT_STANDARD, "%ls payload from working path '%ls' to path '%ls'", fMove ? L"Moving" : L"Copying", wzUnverifiedPayloadPath, wzCachedPath);
 
+    if (fMove)
+    {
+        ReleaseFileHandle(hFile);
+    }
     hr = CacheTransferFileWithRetry(wzUnverifiedPayloadPath, wzCachedPath, fMove, BURN_CACHE_STEP_FINALIZE, pPayload->qwFileSize, pfnCacheMessageHandler, pfnProgress, pContext);
 
 LExit:
@@ -1870,7 +1878,7 @@ static HRESULT VerifyFileAgainstContainer(
     HANDLE hFile = INVALID_HANDLE_VALUE;
 
     // Get the container on disk actual hash.
-    hFile = ::CreateFileW(wzVerifyPath, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_DELETE, NULL, OPEN_EXISTING, FILE_FLAG_SEQUENTIAL_SCAN, NULL);
+    hFile = ::CreateFileW(wzVerifyPath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_FLAG_SEQUENTIAL_SCAN, NULL);
     if (INVALID_HANDLE_VALUE == hFile)
     {
         hr = HRESULT_FROM_WIN32(::GetLastError());
@@ -1892,6 +1900,7 @@ static HRESULT VerifyFileAgainstContainer(
         break;
     }
 
+    ReleaseFileHandle(hFile);
     if (fAlreadyCached)
     {
         LogId(REPORT_STANDARD, MSG_VERIFIED_EXISTING_CONTAINER, pContainer->sczId, wzVerifyPath);
@@ -1929,7 +1938,7 @@ static HRESULT VerifyFileAgainstPayload(
     BOOL fVerifyFileSize = FALSE;
 
     // Get the payload on disk actual hash.
-    hFile = ::CreateFileW(wzVerifyPath, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_DELETE, NULL, OPEN_EXISTING, FILE_FLAG_SEQUENTIAL_SCAN, NULL);
+    hFile = ::CreateFileW(wzVerifyPath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_FLAG_SEQUENTIAL_SCAN, NULL);
     if (INVALID_HANDLE_VALUE == hFile)
     {
         hr = HRESULT_FROM_WIN32(::GetLastError());
@@ -1973,6 +1982,7 @@ static HRESULT VerifyFileAgainstPayload(
         break;
     }
 
+    ReleaseFileHandle(hFile);
     if (fAlreadyCached)
     {
         LogId(REPORT_STANDARD, MSG_VERIFIED_EXISTING_PAYLOAD, pPayload->sczKey, wzVerifyPath);
@@ -2188,7 +2198,7 @@ static HRESULT CopyEngineWithSignatureFixup(
     LARGE_INTEGER li = { };
     DWORD dwZeroOriginals[3] = { };
 
-    hTarget = ::CreateFileW(wzTargetPath, GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_DELETE, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL | FILE_FLAG_SEQUENTIAL_SCAN, NULL);
+    hTarget = ::CreateFileW(wzTargetPath, GENERIC_WRITE, FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL | FILE_FLAG_SEQUENTIAL_SCAN, NULL);
     if (INVALID_HANDLE_VALUE == hTarget)
     {
         ExitWithLastError(hr, "Failed to create engine file at path: %ls", wzTargetPath);
