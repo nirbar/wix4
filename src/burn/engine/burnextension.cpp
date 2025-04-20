nirbar/wix4-1

Extensions can create and extract containers using bundle extensions.

src/burn/engine/burnextension.cpp
@@ -13,7 +13,7 @@ static HRESULT SendRequiredBextMessage(
 // function definitions
 
 /*******************************************************************
- BurnExtensionParseFromXml - 
+ BurnExtensionParseFromXml -
 
 *******************************************************************/
 EXTERN_C HRESULT BurnExtensionParseFromXml(
@@ -81,7 +81,7 @@ LExit:
 }
 
 /*******************************************************************
- BurnExtensionUninitialize - 
+ BurnExtensionUninitialize -
 
 *******************************************************************/
 EXTERN_C void BurnExtensionUninitialize(
@@ -104,7 +104,7 @@ EXTERN_C void BurnExtensionUninitialize(
 }
 
 /*******************************************************************
- BurnExtensionLoad - 
+ BurnExtensionLoad -
 
 *******************************************************************/
 EXTERN_C HRESULT BurnExtensionLoad(
@@ -165,7 +165,7 @@ LExit:
 }
 
 /*******************************************************************
- BurnExtensionUnload - 
+ BurnExtensionUnload -
 
 *******************************************************************/
 EXTERN_C void BurnExtensionUnload(
@@ -245,6 +245,208 @@ EXTERN_C BEEAPI BurnExtensionPerformSearch(
 
     hr = SendRequiredBextMessage(pExtension, BUNDLE_EXTENSION_MESSAGE_SEARCH, &args, &results);
     ExitOnFailure(hr, "BundleExtension '%ls' Search '%ls' failed.", pExtension->sczId, wzSearchId);
+
+LExit:
+    return hr;
+}
+
+EXTERN_C BEEAPI BurnExtensionContainerOpen(
+    __in BURN_EXTENSION* pExtension,
+    __in LPCWSTR wzContainerId,
+    __in LPCWSTR wzFilePath,
+    __in BURN_CONTAINER_CONTEXT* pContext
+)
+{
+    HRESULT hr = S_OK;
+    BUNDLE_EXTENSION_CONTAINER_OPEN_ARGS args = { };
+    BUNDLE_EXTENSION_CONTAINER_OPEN_RESULTS results = { };
+
+    args.cbSize = sizeof(args);
+    args.wzContainerId = wzContainerId;
+    args.wzFilePath = wzFilePath;
+
+    results.cbSize = sizeof(results);
+
+    hr = SendRequiredBextMessage(pExtension, BUNDLE_EXTENSION_MESSAGE_CONTAINER_OPEN, &args, &results);
+    ExitOnFailure(hr, "BundleExtension '%ls' open container '%ls' failed.", pExtension->sczId, wzFilePath);
+
+    pContext->Bex.pExtensionContext = results.pContext;
+
+LExit:
+    return hr;
+}
+
+BEEAPI BurnExtensionContainerNextStream(
+    __in BURN_EXTENSION* pExtension,
+    __in BURN_CONTAINER_CONTEXT* pContext,
+    __inout_z LPWSTR* psczStreamName
+)
+{
+    HRESULT hr = S_OK;
+    BUNDLE_EXTENSION_CONTAINER_NEXT_STREAM_ARGS args = { };
+    BUNDLE_EXTENSION_CONTAINER_NEXT_STREAM_RESULTS results = { };
+    BSTR sczStreamName = nullptr;
+
+    if (psczStreamName && *psczStreamName)
+    {
+        sczStreamName = ::SysAllocString(*psczStreamName);
+        ExitOnNull(sczStreamName, hr, E_FAIL, "Failed to allocate sys string");
+    }
+
+    args.cbSize = sizeof(args);
+    args.pContext = pContext->Bex.pExtensionContext;
+
+    results.cbSize = sizeof(results);
+    results.psczStreamName = &sczStreamName;
+
+    hr = SendRequiredBextMessage(pExtension, BUNDLE_EXTENSION_MESSAGE_CONTAINER_NEXT_STREAM, &args, &results);
+    if (hr != E_NOMOREITEMS)
+    {
+        ExitOnFailure(hr, "BundleExtension '%ls' failed to move to next stream.", pExtension->sczId);
+
+        if (psczStreamName)
+        {
+            if (sczStreamName)
+            {
+                hr = StrAllocString(psczStreamName, sczStreamName, 0);
+                ExitOnFailure(hr, "Failed to copy string");
+            }
+            else
+            {
+                ReleaseNullStr(*psczStreamName);
+            }
+        }
+    }
+
+LExit:
+    if (sczStreamName)
+    {
+        ::SysFreeString(sczStreamName);
+    }
+
+    return hr;
+}
+
+BEEAPI BurnExtensionContainerStreamToFile(
+    __in BURN_EXTENSION* pExtension,
+    __in BURN_CONTAINER_CONTEXT* pContext,
+    __in_z LPCWSTR wzFileName
+)
+{
+    HRESULT hr = S_OK;
+    BUNDLE_EXTENSION_CONTAINER_STREAM_TO_FILE_ARGS args = { };
+    BUNDLE_EXTENSION_CONTAINER_STREAM_TO_FILE_RESULTS results = { };
+
+    args.cbSize = sizeof(args);
+    args.pContext = pContext->Bex.pExtensionContext;
+    args.wzFileName = wzFileName;
+
+    results.cbSize = sizeof(results);
+
+    hr = SendRequiredBextMessage(pExtension, BUNDLE_EXTENSION_MESSAGE_CONTAINER_STREAM_TO_FILE, &args, &results);
+    ExitOnFailure(hr, "BundleExtension '%ls' failed to extract file '%ls'.", pExtension->sczId, wzFileName);
+
+LExit:
+    return hr;
+}
+
+BEEAPI BurnExtensionContainerStreamToBuffer(
+    __in BURN_EXTENSION* pExtension,
+    __in BURN_CONTAINER_CONTEXT* pContext,
+    __inout LPBYTE* ppbBuffer,
+    __inout SIZE_T* pcbBuffer
+)
+{
+    HRESULT hr = S_OK;
+    BUNDLE_EXTENSION_CONTAINER_STREAM_TO_BUFFER_ARGS args = { };
+    BUNDLE_EXTENSION_CONTAINER_STREAM_TO_BUFFER_RESULTS results = { };
+    LPBYTE pbBuffer = nullptr;
+    SIZE_T cbBuffer = 0;
+    errno_t err = 0;
+
+    args.cbSize = sizeof(args);
+    args.pContext = pContext->Bex.pExtensionContext;
+
+    results.cbSize = sizeof(results);
+    results.ppbBuffer = &pbBuffer;
+    results.pcbBuffer = &cbBuffer;
+
+    hr = SendRequiredBextMessage(pExtension, BUNDLE_EXTENSION_MESSAGE_CONTAINER_STREAM_TO_BUFFER, &args, &results);
+    ExitOnFailure(hr, "BundleExtension '%ls' failed to extract stream to buffer.", pExtension->sczId);
+
+    if (pbBuffer)
+    {
+        if (ppbBuffer && *ppbBuffer)
+        {
+            LPVOID pv = MemReAlloc(*ppbBuffer, cbBuffer, FALSE);
+            ExitOnNull(pv, hr, E_OUTOFMEMORY, "Failed to reallocate memory.");
+
+            *ppbBuffer = (LPBYTE)pv;
+            *pcbBuffer = cbBuffer;
+        }
+        else
+        {
+            *ppbBuffer = (LPBYTE)MemAlloc(cbBuffer, FALSE);
+            ExitOnNull(*ppbBuffer, hr, E_OUTOFMEMORY, "Failed to allocate memory.");
+
+            *pcbBuffer = cbBuffer;
+        }
+
+        err = ::memcpy_s(*ppbBuffer, cbBuffer, pbBuffer, cbBuffer);
+        ExitOnNull(!err, hr, HRESULT_FROM_WIN32(err), "Failed to copy memory");
+    }
+    else if (ppbBuffer && *ppbBuffer)
+    {
+        ReleaseNullMem(*ppbBuffer);
+        *pcbBuffer = 0;
+    }
+
+LExit:
+    if (pbBuffer)
+    {
+        ::CoTaskMemFree(pbBuffer);
+    }
+
+    return hr;
+}
+
+BEEAPI BurnExtensionContainerSkipStream(
+    __in BURN_EXTENSION* pExtension,
+    __in BURN_CONTAINER_CONTEXT* pContext
+)
+{
+    HRESULT hr = S_OK;
+    BUNDLE_EXTENSION_CONTAINER_STREAM_TO_BUFFER_ARGS args = { };
+    BUNDLE_EXTENSION_CONTAINER_STREAM_TO_BUFFER_RESULTS results = { };
+
+    args.cbSize = sizeof(args);
+    args.pContext = pContext->Bex.pExtensionContext;
+
+    results.cbSize = sizeof(results);
+
+    hr = SendRequiredBextMessage(pExtension, BUNDLE_EXTENSION_MESSAGE_CONTAINER_SKIP_STREAM, &args, &results);
+    ExitOnFailure(hr, "BundleExtension '%ls' failed to skip stream.", pExtension->sczId);
+
+LExit:
+    return hr;
+}
+
+BEEAPI BurnExtensionContainerClose(
+    __in BURN_EXTENSION* pExtension,
+    __in BURN_CONTAINER_CONTEXT* pContext
+)
+{
+    HRESULT hr = S_OK;
+    BUNDLE_EXTENSION_CONTAINER_CLOSE_ARGS args = { };
+    BUNDLE_EXTENSION_CONTAINER_CLOSE_RESULTS results = { };
+
+    args.cbSize = sizeof(args);
+    args.pContext = pContext->Bex.pExtensionContext;
+
+    results.cbSize = sizeof(results);
+
+    hr = SendRequiredBextMessage(pExtension, BUNDLE_EXTENSION_MESSAGE_CONTAINER_CLOSE, &args, &results);
+    ExitOnFailure(hr, "BundleExtension '%ls' failed to close container.", pExtension->sczId);
 
 LExit:
     return hr;

src/wix/WixToolset.Core.Burn/Bundles/CreateBurnManifestCommand.cs
@@ -752,6 +752,13 @@ namespace WixToolset.Core.Burn.Bundles
                 writer.WriteAttributeString("Attached", "yes");
                 writer.WriteAttributeString("Primary", "yes");
             }
+
+            // Extension container
+            if (!String.IsNullOrEmpty(container.BundleExtensionRef))
+            {
+                writer.WriteAttributeString("Type", "Extension");
+                writer.WriteAttributeString("ExtensionId", container.BundleExtensionRef);
+            }
         }
 
         private void WriteBurnManifestPayload(XmlTextWriter writer, WixBundlePayloadSymbol payload)

nirbar/wix4

src/burn/engine/burnextension.cpp
@@ -250,6 +250,236 @@ LExit:
     return hr;
 }
 
+EXTERN_C BEEAPI BurnExtensionContainerOpen(
+    __in BURN_EXTENSION* pExtension,
+    __in LPCWSTR wzContainerId,
+    __in LPCWSTR wzFilePath,
+    __in BURN_CONTAINER_CONTEXT* pContext
+)
+{
+    HRESULT hr = S_OK;
+    BOOTSTRAPPER_EXTENSION_CONTAINER_OPEN_ARGS args = { };
+    BOOTSTRAPPER_EXTENSION_CONTAINER_OPEN_RESULTS results = { };
+
+    args.cbSize = sizeof(args);
+    args.wzContainerId = wzContainerId;
+    args.wzFilePath = wzFilePath;
+
+    results.cbSize = sizeof(results);
+
+    hr = SendRequiredBextMessage(pExtension, BOOTSTRAPPER_EXTENSION_MESSAGE_CONTAINER_OPEN, &args, &results);
+    pContext->Bex.pExtensionContext = results.pContext;
+    ExitOnFailure(hr, "BundleExtension '%ls' open container '%ls' failed.", pExtension->sczId, wzFilePath);
+
+LExit:
+    return hr;
+}
+
+EXTERN_C BEEAPI BurnExtensionContainerOpenAttached(
+    __in BURN_EXTENSION* pExtension,
+    __in LPCWSTR wzContainerId,
+    __in HANDLE hBundle,
+    __in DWORD64 qwContainerStartPos,
+    __in DWORD64 qwContainerSize,
+    __in BURN_CONTAINER_CONTEXT* pContext
+)
+{
+    HRESULT hr = S_OK;
+    BOOTSTRAPPER_EXTENSION_CONTAINER_OPEN_ATTACHED_ARGS args = { };
+    BOOTSTRAPPER_EXTENSION_CONTAINER_OPEN_ATTACHED_RESULTS results = { };
+
+    args.cbSize = sizeof(args);
+    args.wzContainerId = wzContainerId;
+    args.hBundle = hBundle;
+    args.qwContainerStartPos = qwContainerStartPos;
+    args.qwContainerSize = qwContainerSize;
+
+    results.cbSize = sizeof(results);
+
+    hr = SendRequiredBextMessage(pExtension, BOOTSTRAPPER_EXTENSION_MESSAGE_CONTAINER_OPEN_ATTACHED, &args, &results);
+    pContext->Bex.pExtensionContext = results.pContext;
+    ExitOnFailure(hr, "BundleExtension '%ls' open attached container failed.", pExtension->sczId);
+
+LExit:
+    return hr;
+}
+
+BEEAPI BurnExtensionContainerNextStream(
+    __in BURN_EXTENSION* pExtension,
+    __in BURN_CONTAINER_CONTEXT* pContext,
+    __inout_z LPWSTR* psczStreamName
+)
+{
+    HRESULT hr = S_OK;
+    BOOTSTRAPPER_EXTENSION_CONTAINER_NEXT_STREAM_ARGS args = { };
+    BOOTSTRAPPER_EXTENSION_CONTAINER_NEXT_STREAM_RESULTS results = { };
+    BSTR sczStreamName = nullptr;
+
+    if (psczStreamName && *psczStreamName)
+    {
+        sczStreamName = ::SysAllocString(*psczStreamName);
+        ExitOnNull(sczStreamName, hr, E_FAIL, "Failed to allocate sys string");
+    }
+
+    args.cbSize = sizeof(args);
+    args.pContext = pContext->Bex.pExtensionContext;
+
+    results.cbSize = sizeof(results);
+    results.psczStreamName = &sczStreamName;
+
+    hr = SendRequiredBextMessage(pExtension, BOOTSTRAPPER_EXTENSION_MESSAGE_CONTAINER_NEXT_STREAM, &args, &results);
+    if (hr != E_NOMOREITEMS)
+    {
+        ExitOnFailure(hr, "BundleExtension '%ls' failed to move to next stream.", pExtension->sczId);
+
+        if (psczStreamName)
+        {
+            if (sczStreamName)
+            {
+                hr = StrAllocString(psczStreamName, sczStreamName, 0);
+                ExitOnFailure(hr, "Failed to copy string");
+            }
+            else
+            {
+                ReleaseNullStr(*psczStreamName);
+            }
+        }
+    }
+
+LExit:
+    if (sczStreamName)
+    {
+        ::SysFreeString(sczStreamName);
+    }
+
+    return hr;
+}
+
+BEEAPI BurnExtensionContainerStreamToFile(
+    __in BURN_EXTENSION* pExtension,
+    __in BURN_CONTAINER_CONTEXT* pContext,
+    __in_z LPCWSTR wzFileName
+)
+{
+    HRESULT hr = S_OK;
+    BOOTSTRAPPER_EXTENSION_CONTAINER_STREAM_TO_FILE_ARGS args = { };
+    BOOTSTRAPPER_EXTENSION_CONTAINER_STREAM_TO_FILE_RESULTS results = { };
+
+    args.cbSize = sizeof(args);
+    args.pContext = pContext->Bex.pExtensionContext;
+    args.wzFileName = wzFileName;
+
+    results.cbSize = sizeof(results);
+
+    hr = SendRequiredBextMessage(pExtension, BOOTSTRAPPER_EXTENSION_MESSAGE_CONTAINER_STREAM_TO_FILE, &args, &results);
+    ExitOnFailure(hr, "BundleExtension '%ls' failed to extract file '%ls'.", pExtension->sczId, wzFileName);
+
+LExit:
+    return hr;
+}
+
+BEEAPI BurnExtensionContainerStreamToBuffer(
+    __in BURN_EXTENSION* pExtension,
+    __in BURN_CONTAINER_CONTEXT* pContext,
+    __inout LPBYTE* ppbBuffer,
+    __inout SIZE_T* pcbBuffer
+)
+{
+    HRESULT hr = S_OK;
+    BOOTSTRAPPER_EXTENSION_CONTAINER_STREAM_TO_BUFFER_ARGS args = { };
+    BOOTSTRAPPER_EXTENSION_CONTAINER_STREAM_TO_BUFFER_RESULTS results = { };
+    LPBYTE pbBuffer = nullptr;
+    SIZE_T cbBuffer = 0;
+    errno_t err = 0;
+
+    args.cbSize = sizeof(args);
+    args.pContext = pContext->Bex.pExtensionContext;
+
+    results.cbSize = sizeof(results);
+    results.ppbBuffer = &pbBuffer;
+    results.pcbBuffer = &cbBuffer;
+
+    hr = SendRequiredBextMessage(pExtension, BOOTSTRAPPER_EXTENSION_MESSAGE_CONTAINER_STREAM_TO_BUFFER, &args, &results);
+    ExitOnFailure(hr, "BundleExtension '%ls' failed to extract stream to buffer.", pExtension->sczId);
+
+    if (pbBuffer)
+    {
+        if (ppbBuffer && *ppbBuffer)
+        {
+            LPVOID pv = MemReAlloc(*ppbBuffer, cbBuffer, FALSE);
+            ExitOnNull(pv, hr, E_OUTOFMEMORY, "Failed to reallocate memory.");
+
+            *ppbBuffer = (LPBYTE)pv;
+            *pcbBuffer = cbBuffer;
+        }
+        else
+        {
+            *ppbBuffer = (LPBYTE)MemAlloc(cbBuffer, FALSE);
+            ExitOnNull(*ppbBuffer, hr, E_OUTOFMEMORY, "Failed to allocate memory.");
+
+            *pcbBuffer = cbBuffer;
+        }
+
+        err = ::memcpy_s(*ppbBuffer, cbBuffer, pbBuffer, cbBuffer);
+        ExitOnNull(!err, hr, HRESULT_FROM_WIN32(err), "Failed to copy memory");
+    }
+    else if (ppbBuffer && *ppbBuffer)
+    {
+        ReleaseNullMem(*ppbBuffer);
+        *pcbBuffer = 0;
+    }
+
+LExit:
+    if (pbBuffer)
+    {
+        ::CoTaskMemFree(pbBuffer);
+    }
+
+    return hr;
+}
+
+BEEAPI BurnExtensionContainerSkipStream(
+    __in BURN_EXTENSION* pExtension,
+    __in BURN_CONTAINER_CONTEXT* pContext
+)
+{
+    HRESULT hr = S_OK;
+    BOOTSTRAPPER_EXTENSION_CONTAINER_STREAM_TO_BUFFER_ARGS args = { };
+    BOOTSTRAPPER_EXTENSION_CONTAINER_STREAM_TO_BUFFER_RESULTS results = { };
+
+    args.cbSize = sizeof(args);
+    args.pContext = pContext->Bex.pExtensionContext;
+
+    results.cbSize = sizeof(results);
+
+    hr = SendRequiredBextMessage(pExtension, BOOTSTRAPPER_EXTENSION_MESSAGE_CONTAINER_SKIP_STREAM, &args, &results);
+    ExitOnFailure(hr, "BundleExtension '%ls' failed to skip stream.", pExtension->sczId);
+
+LExit:
+    return hr;
+}
+
+BEEAPI BurnExtensionContainerClose(
+    __in BURN_EXTENSION* pExtension,
+    __in BURN_CONTAINER_CONTEXT* pContext
+)
+{
+    HRESULT hr = S_OK;
+    BOOTSTRAPPER_EXTENSION_CONTAINER_CLOSE_ARGS args = { };
+    BOOTSTRAPPER_EXTENSION_CONTAINER_CLOSE_RESULTS results = { };
+
+    args.cbSize = sizeof(args);
+    args.pContext = pContext->Bex.pExtensionContext;
+
+    results.cbSize = sizeof(results);
+
+    hr = SendRequiredBextMessage(pExtension, BOOTSTRAPPER_EXTENSION_MESSAGE_CONTAINER_CLOSE, &args, &results);
+    ExitOnFailure(hr, "BundleExtension '%ls' failed to close container.", pExtension->sczId);
+
+LExit:
+    return hr;
+}
+
 static HRESULT SendRequiredBextMessage(
     __in BURN_EXTENSION* pExtension,
     __in BOOTSTRAPPER_EXTENSION_MESSAGE message,

src/wix/WixToolset.Core.Burn/Bundles/CreateBurnManifestCommand.cs
@@ -758,6 +758,13 @@ namespace WixToolset.Core.Burn.Bundles
                 writer.WriteAttributeString("Attached", "yes");
                 writer.WriteAttributeString("Primary", "yes");
             }
+
+            // Extension container
+            if (!String.IsNullOrEmpty(container.BootstrapperExtensionRef))
+            {
+                writer.WriteAttributeString("Type", "Extension");
+                writer.WriteAttributeString("ExtensionId", container.BootstrapperExtensionRef);
+            }
         }
 
         private void WriteBurnManifestPayload(XmlTextWriter writer, WixBundlePayloadSymbol payload)