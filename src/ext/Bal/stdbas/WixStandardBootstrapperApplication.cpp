0896b6b6d9170104219a922cbcac86ecdd4a97da
Nir Bar <nir.bar@panel-sw.co.il>
2025-12-31 10:30:51 +0200

    Fix the MSI files-in-use label text

--- src/ext/Bal/stdbas/WixStandardBootstrapperApplication.cpp
+++ src/ext/Bal/stdbas/WixStandardBootstrapperApplication.cpp
@@ -2489,7 +2489,7 @@ private: // privates
         hr = BalFormatString(m_pFilesInUseTitleLoc->wzText, &sczTitle);
         BalExitOnFailure(hr, "Failed to format FilesInUseTitle loc string.");
 
-        hr = BalFormatString(m_pFilesInUseLabelLoc->wzText, &sczLabel);
+        hr = BalFormatString(m_pRmFilesInUseLabelLoc->wzText, &sczLabel);
         BalExitOnFailure(hr, "Failed to format FilesInUseLabel loc string.");
 
         hr = BalFormatString(m_pFilesInUseCloseRadioButtonLoc->wzText, &sczCloseRadioButton);
@@ -2551,7 +2551,7 @@ private: // privates
         hr = BalFormatString(m_pFilesInUseTitleLoc->wzText, &sczTitle);
         BalExitOnFailure(hr, "Failed to format FilesInUseTitle loc string.");
 
-        hr = BalFormatString(m_pFilesInUseLabelLoc->wzText, &sczLabel);
+        hr = BalFormatString(m_pMsiFilesInUseLabelLoc->wzText, &sczLabel);
         BalExitOnFailure(hr, "Failed to format FilesInUseLabel loc string.");
 
         hr = BalFormatString(m_pFilesInUseRetryButtonLoc->wzText, &sczRetryButton);
@@ -2665,7 +2665,7 @@ private: // privates
         hr = BalFormatString(m_pFilesInUseTitleLoc->wzText, &sczTitle);
         BalExitOnFailure(hr, "Failed to format FilesInUseTitle loc string.");
 
-        hr = BalFormatString(m_pFilesInUseLabelLoc->wzText, &sczLabel);
+        hr = BalFormatString(m_pRmFilesInUseLabelLoc->wzText, &sczLabel);
         BalExitOnFailure(hr, "Failed to format FilesInUseLabel loc string.");
 
         hr = BalFormatString(m_pFilesInUseNetfxCloseRadioButtonLoc->wzText, &sczNetfxCloseRadioButton);
@@ -3050,7 +3050,11 @@ private:
     {
         // Get the loc strings for the files-in-use dialogs.
         LoadIndividualLocString(L"#(loc.FilesInUseTitle)", &m_pFilesInUseTitleLoc);
-        LoadIndividualLocString(L"#(loc.FilesInUseLabel)", &m_pFilesInUseLabelLoc);
+        LoadIndividualLocString(L"#(loc.FilesInUseLabel)", &m_pRmFilesInUseLabelLoc);
+        if (E_NOTFOUND == LoadIndividualLocString(L"#(loc.MsiFilesInUseLabel)", &m_pMsiFilesInUseLabelLoc))
+        {
+            LoadIndividualLocString(L"#(loc.FilesInUseLabel)", &m_pMsiFilesInUseLabelLoc);
+        }
         LoadIndividualLocString(L"#(loc.FilesInUseCloseRadioButton)", &m_pFilesInUseCloseRadioButtonLoc);
         LoadIndividualLocString(L"#(loc.FilesInUseDontCloseRadioButton)", &m_pFilesInUseDontCloseRadioButtonLoc);
         LoadIndividualLocString(L"#(loc.FilesInUseRetryButton)", &m_pFilesInUseRetryButtonLoc);
@@ -3058,9 +3062,9 @@ private:
         LoadIndividualLocString(L"#(loc.FilesInUseExitButton)", &m_pFilesInUseExitButtonLoc);
         LoadIndividualLocString(L"#(loc.FilesInUseNetfxCloseRadioButton)", &m_pFilesInUseNetfxCloseRadioButtonLoc);
 
-        m_fShowRMFilesInUse = m_pFilesInUseTitleLoc && m_pFilesInUseLabelLoc && m_pFilesInUseCloseRadioButtonLoc && m_pFilesInUseDontCloseRadioButtonLoc;
-        m_fShowStandardFilesInUse = m_pFilesInUseTitleLoc && m_pFilesInUseLabelLoc && m_pFilesInUseRetryButtonLoc && m_pFilesInUseIgnoreButtonLoc && m_pFilesInUseExitButtonLoc;
-        m_fShowNetfxFilesInUse = m_pFilesInUseTitleLoc && m_pFilesInUseLabelLoc && m_pFilesInUseNetfxCloseRadioButtonLoc && m_pFilesInUseDontCloseRadioButtonLoc;
+        m_fShowRMFilesInUse = m_pFilesInUseTitleLoc && m_pRmFilesInUseLabelLoc && m_pFilesInUseCloseRadioButtonLoc && m_pFilesInUseDontCloseRadioButtonLoc;
+        m_fShowStandardFilesInUse = m_pFilesInUseTitleLoc && m_pMsiFilesInUseLabelLoc && m_pFilesInUseRetryButtonLoc && m_pFilesInUseIgnoreButtonLoc && m_pFilesInUseExitButtonLoc;
+        m_fShowNetfxFilesInUse = m_pFilesInUseTitleLoc && m_pRmFilesInUseLabelLoc && m_pFilesInUseNetfxCloseRadioButtonLoc && m_pFilesInUseDontCloseRadioButtonLoc;
     }
 
 
@@ -4857,7 +4861,8 @@ public:
         m_nLastMsiFilesInUseResult = IDNOACTION;
         m_nLastNetfxFilesInUseResult = IDNOACTION;
         m_pFilesInUseTitleLoc = NULL;
-        m_pFilesInUseLabelLoc = NULL;
+        m_pMsiFilesInUseLabelLoc = NULL;
+        m_pRmFilesInUseLabelLoc = NULL;
         m_pFilesInUseCloseRadioButtonLoc = NULL;
         m_pFilesInUseNetfxCloseRadioButtonLoc = NULL;
         m_pFilesInUseDontCloseRadioButtonLoc = NULL;
@@ -5190,7 +5195,8 @@ private:
     int m_nLastMsiFilesInUseResult;
     int m_nLastNetfxFilesInUseResult;
     LOC_STRING* m_pFilesInUseTitleLoc;
-    LOC_STRING* m_pFilesInUseLabelLoc;
+    LOC_STRING* m_pRmFilesInUseLabelLoc;
+    LOC_STRING* m_pMsiFilesInUseLabelLoc;
     LOC_STRING* m_pFilesInUseCloseRadioButtonLoc;
     LOC_STRING* m_pFilesInUseNetfxCloseRadioButtonLoc;
     LOC_STRING* m_pFilesInUseDontCloseRadioButtonLoc;
