nirbar/wix4-1

Extensions can create and extract containers using bundle extensions.

src/wix/WixToolset.Core.Burn/Bundles/CreateBurnManifestCommand.cs
@@ -752,6 +752,13 @@ namespace WixToolset.Core.Burn.Bundles
                 writer.WriteAttributeString("Attached", "yes");
                 writer.WriteAttributeString("Primary", "yes");
             }
+
+            // Extension container
+            if (!String.IsNullOrEmpty(container.BundleExtensionRef))
+            {
+                writer.WriteAttributeString("Type", "Extension");
+                writer.WriteAttributeString("ExtensionId", container.BundleExtensionRef);
+            }
         }
 
         private void WriteBurnManifestPayload(XmlTextWriter writer, WixBundlePayloadSymbol payload)

nirbar/wix4

src/wix/WixToolset.Core.Burn/Bundles/CreateBurnManifestCommand.cs
@@ -762,6 +762,13 @@ namespace WixToolset.Core.Burn.Bundles
                 writer.WriteAttributeString("Attached", "yes");
                 writer.WriteAttributeString("Primary", "yes");
             }
+
+            // Extension container
+            if (!String.IsNullOrEmpty(container.BootstrapperExtensionRef))
+            {
+                writer.WriteAttributeString("Type", "Extension");
+                writer.WriteAttributeString("ExtensionId", container.BootstrapperExtensionRef);
+            }
         }
 
         private void WriteBurnManifestPayload(XmlTextWriter writer, WixBundlePayloadSymbol payload)