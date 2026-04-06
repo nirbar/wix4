nirbar/wix4-1

Bundle/@Wix3DependencyMode="yes/no". If 'yes' then default MSI/MSP provider key is the ProductCode/PatchCode. Use only if a WiX4 bundle chains packages of a WiX3 bundle. The default is 'no'.

src/api/wix/WixToolset.Data/Symbols/WixBundleSymbol.cs
@@ -79,6 +79,7 @@ namespace WixToolset.Data.Symbols
         None = 0x0,
         DisableRemove = 0x1,
         PerMachine = 0x2,
+        Wix3DependencyMode = 0x4,
     }
 
     public enum WixBundleModifyType
@@ -288,6 +289,22 @@ namespace WixToolset.Data.Symbols
                 else
                 {
                     this.Attributes &= ~WixBundleAttributes.PerMachine;
+                }
+            }
+        }
+
+        public bool Wix3DependencyMode
+        {
+            get { return this.Attributes.HasFlag(WixBundleAttributes.Wix3DependencyMode); }
+            set
+            {
+                if (value)
+                {
+                    this.Attributes |= WixBundleAttributes.Wix3DependencyMode;
+                }
+                else
+                {
+                    this.Attributes &= ~WixBundleAttributes.Wix3DependencyMode;
                 }
             }
         }

nirbar/wix4

src/api/wix/WixToolset.Data/Symbols/WixBundleSymbol.cs
@@ -82,6 +82,7 @@ namespace WixToolset.Data.Symbols
         None = 0x0,
         DisableRemove = 0x1,
         RunAsAdmin = 0x4,
+        Wix3DependencyMode = 0x8,
     }
 
     public enum WixBundleModifyType
@@ -328,6 +329,22 @@ namespace WixToolset.Data.Symbols
                 else
                 {
                     this.Attributes &= ~WixBundleAttributes.RunAsAdmin;
+                }
+            }
+        }
+
+        public bool Wix3DependencyMode
+        {
+            get { return this.Attributes.HasFlag(WixBundleAttributes.Wix3DependencyMode); }
+            set
+            {
+                if (value)
+                {
+                    this.Attributes |= WixBundleAttributes.Wix3DependencyMode;
+                }
+                else
+                {
+                    this.Attributes &= ~WixBundleAttributes.Wix3DependencyMode;
                 }
             }
         }